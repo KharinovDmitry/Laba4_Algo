2798c5dce8e9292abb9db8fc7e84605dc2138c30
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-11-13 00:24:11 +0500

    TextSort done

---
+++ TextSort/Analyzer.cs
@@ -0,0 +1,43 @@
+﻿using Core.TextSort;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSort
+{
+    internal static class Analyzer
+    {
+        public const string pathToTexts = "..\\..\\..\\..\\Core\\TextSort\\Texts";
+        public static string[] fileNames = new string[]
+        {
+            "100words.txt",
+            "500words.txt",
+            "1000words.txt",
+            "2000words.txt",
+            "5000words.txt",
+        };
+
+        public static AnalyzeResult Evaluate(BaseTextSort textSort)
+        {
+            string algoName = textSort.GetType().Name;
+            AnalyzeResult report = new AnalyzeResult(algoName);
+
+            foreach (string fileName in TxtReader.fileNames)
+            {
+                string text = TxtReader.ReadText(fileName);
+
+                Stopwatch sw = Stopwatch.StartNew();
+                sw.Start();
+                string[] words = textSort.Sort(text);
+                sw.Stop();
+
+                report.AddMeasurement(words.Length, sw.Elapsed.TotalSeconds);
+            }
+
+            return report;
+        }
+    }
+}
---
+++ TextSort/CsvWriter.cs
@@ -0,0 +1,57 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSort
+{
+    internal class CsvWriter
+    {
+        public static void WriteAnalyzeResult(AnalyzeResult result)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.Commercial;
+            
+            FileInfo file = new FileInfo("Charts.xlsx");
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet? worksheet = package.Workbook.Worksheets.FirstOrDefault(x => x.Name == result.Title);
+                if (worksheet == null)
+                {
+                    worksheet = package.Workbook.Worksheets.Add(result.Title);
+                }
+                else
+                {
+                    worksheet.Cells.Clear();
+                }
+
+                worksheet.Cells[1, 1].Value = "ArrLength";
+                worksheet.Cells[1, 2].Value = "Time";
+
+                Console.WriteLine($"> Algorithm {result.Title} run!\n");
+                Console.WriteLine("Count of words | Time in seconds");
+                Console.WriteLine("--------------------------------");
+
+                worksheet.Cells[1, 1].Value = "ArrLength";
+                worksheet.Cells[1, 2].Value = "Time";
+
+                for (int i = 0; i < result.Measurements.Count; i++)
+                {
+                    var ArrLength = result.Measurements[i].ArrayLength;
+                    var Time = result.Measurements[i].TimeInS;
+
+                    Console.Write($"{ArrLength}{string.Join("", Enumerable.Repeat(" ", 15 - ArrLength.ToString().Length))}|");
+                    Console.WriteLine($" {Time}");
+
+                    worksheet.Cells[i + 2, 1].Value = ArrLength;
+                    worksheet.Cells[i + 2, 2].Value = Time;
+                }
+
+                Console.WriteLine($"\n> Algorithm {result.Title} done!\n");
+                package.SaveAs(file);
+            }
+        }
+    }
+}
